debef87f612b080454ebd362cf43e570599605fa
noah-stahl <noah.stahl@mindmatters.de>
2023-06-30 10:40:26 +0200

    organize components

---
+++ src/components/FretButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+  noteName: String
+})
+</script>
+
+<template>
+  <button class="fretButton border-2">{{ noteName }}</button>
+</template>
+
+
+<style>
+.fretButton {
+  width: 50px;
+  height: 50px;
+}
+</style>
---
+++ src/views/QuizView.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import FretButton from "../components/FretButton.vue"
+</script>
+
+<template>
+  <div>
+    <h1>Fretboard</h1>
+    <div>
+      <FretButton note-name="E" />
+      <FretButton note-name="D#" />
+      <FretButton note-name="D" />
+      <FretButton note-name="C#" />
+      <FretButton note-name="C" />
+      <FretButton note-name="B" />
+      <FretButton note-name="A#" />
+      <FretButton note-name="A" />
+      <FretButton note-name="G#" />
+      <FretButton note-name="G" />
+      <FretButton note-name="F#" />
+      <FretButton note-name="F" />
+      <FretButton note-name="E" />
+    </div>
+    <div>
+      <FretButton note-name="B" />
+      <FretButton note-name="A#" />
+      <FretButton note-name="A" />
+      <FretButton note-name="G#" />
+      <FretButton note-name="G" />
+      <FretButton note-name="F#" />
+      <FretButton note-name="F" />
+      <FretButton note-name="E" />
+      <FretButton note-name="D#" />
+      <FretButton note-name="D" />
+      <FretButton note-name="C#" />
+      <FretButton note-name="C" />
+      <FretButton note-name="B" />
+    </div>
+    <div>
+      <FretButton note-name="G" />
+      <FretButton note-name="F#" />
+      <FretButton note-name="F" />
+      <FretButton note-name="E" />
+      <FretButton note-name="D#" />
+      <FretButton note-name="D" />
+      <FretButton note-name="C#" />
+      <FretButton note-name="C" />
+      <FretButton note-name="B" />
+      <FretButton note-name="A#" />
+      <FretButton note-name="A" />
+      <FretButton note-name="G#" />
+      <FretButton note-name="G" />
+    </div>
+    <div>
+      <FretButton note-name="D" />
+      <FretButton note-name="C#" />
+      <FretButton note-name="C" />
+      <FretButton note-name="B" />
+      <FretButton note-name="A#" />
+      <FretButton note-name="A" />
+      <FretButton note-name="G#" />
+      <FretButton note-name="G" />
+      <FretButton note-name="F#" />
+      <FretButton note-name="F" />
+      <FretButton note-name="E" />
+      <FretButton note-name="D#" />
+      <FretButton note-name="D" />
+    </div>
+    <div>
+      <FretButton note-name="A" />
+      <FretButton note-name="G#" />
+      <FretButton note-name="G" />
+      <FretButton note-name="F#" />
+      <FretButton note-name="F" />
+      <FretButton note-name="E" />
+      <FretButton note-name="D#" />
+      <FretButton note-name="D" />
+      <FretButton note-name="C#" />
+      <FretButton note-name="C" />
+      <FretButton note-name="B" />
+      <FretButton note-name="A#" />
+      <FretButton note-name="A" />
+    </div>
+    <div>
+      <FretButton note-name="E" />
+      <FretButton note-name="D#" />
+      <FretButton note-name="D" />
+      <FretButton note-name="C#" />
+      <FretButton note-name="C" />
+      <FretButton note-name="B" />
+      <FretButton note-name="A#" />
+      <FretButton note-name="A" />
+      <FretButton note-name="G#" />
+      <FretButton note-name="G" />
+      <FretButton note-name="F#" />
+      <FretButton note-name="F" />
+      <FretButton note-name="E" />
+    </div>
+  </div>
+</template>
