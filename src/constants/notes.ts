47ccbae18ca8bad41bfcec97fc53d684160a9a92
noah-stahl <noah.stahl@mindmatters.de>
2023-07-07 10:36:14 +0200

    Move sounds to assets folder

--- src/constants/notes.ts
+++ src/constants/notes.ts
@@ -5,192 +5,192 @@ import { PitchClass } from '../types/PitchClass'
 const E1: Note = {
   name: 'E1',
   pitchClass: PitchClass.E,
-  sound: new Howl({ src: '../sounds/E1.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/E1.mp3' })
 }
 const F1: Note = {
   name: 'F1',
   pitchClass: PitchClass.F,
-  sound: new Howl({ src: '../sounds/F1.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/F1.mp3' })
 }
 const FSharp1: Note = {
   name: 'F#1',
   pitchClass: PitchClass.FSharp,
-  sound: new Howl({ src: '../sounds/FSharp1.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/FSharp1.mp3' })
 }
 const G1: Note = {
   name: 'G1',
   pitchClass: PitchClass.G,
-  sound: new Howl({ src: '../sounds/G1.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/G1.mp3' })
 }
 const GSharp1: Note = {
   name: 'G#1',
   pitchClass: PitchClass.GSharp,
-  sound: new Howl({ src: '../sounds/GSharp1.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/GSharp1.mp3' })
 }
 const A1: Note = {
   name: 'A1',
   pitchClass: PitchClass.A,
-  sound: new Howl({ src: '../sounds/A1.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/A1.mp3' })
 }
 const ASharp1: Note = {
   name: 'A#1',
   pitchClass: PitchClass.ASharp,
-  sound: new Howl({ src: '../sounds/ASharp1.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/ASharp1.mp3' })
 }
 const B1: Note = {
   name: 'B1',
   pitchClass: PitchClass.B,
-  sound: new Howl({ src: '../sounds/B1.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/B1.mp3' })
 }
 const C2: Note = {
   name: 'C2',
   pitchClass: PitchClass.C,
-  sound: new Howl({ src: '../sounds/C2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/C2.mp3' })
 }
 const CSharp2: Note = {
   name: 'C#2',
   pitchClass: PitchClass.CSharp,
-  sound: new Howl({ src: '../sounds/CSharp2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/CSharp2.mp3' })
 }
 const D2: Note = {
   name: 'D2',
   pitchClass: PitchClass.D,
-  sound: new Howl({ src: '../sounds/D2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/D2.mp3' })
 }
 const DSharp2: Note = {
   name: 'D#2',
   pitchClass: PitchClass.DSharp,
-  sound: new Howl({ src: '../sounds/DSharp2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/DSharp2.mp3' })
 }
 const E2: Note = {
   name: 'E2',
   pitchClass: PitchClass.E,
-  sound: new Howl({ src: '../sounds/E2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/E2.mp3' })
 }
 const F2: Note = {
   name: 'F2',
   pitchClass: PitchClass.F,
-  sound: new Howl({ src: '../sounds/F2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/F2.mp3' })
 }
 const FSharp2: Note = {
   name: 'F#2',
   pitchClass: PitchClass.FSharp,
-  sound: new Howl({ src: '../sounds/FSharp2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/FSharp2.mp3' })
 }
 const G2: Note = {
   name: 'G2',
   pitchClass: PitchClass.G,
-  sound: new Howl({ src: '../sounds/G2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/G2.mp3' })
 }
 const GSharp2: Note = {
   name: 'G#2',
   pitchClass: PitchClass.GSharp,
-  sound: new Howl({ src: '../sounds/GSharp2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/GSharp2.mp3' })
 }
 const A2: Note = {
   name: 'A2',
   pitchClass: PitchClass.A,
-  sound: new Howl({ src: '../sounds/A2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/A2.mp3' })
 }
 const ASharp2: Note = {
   name: 'A#2',
   pitchClass: PitchClass.ASharp,
-  sound: new Howl({ src: '../sounds/ASharp2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/ASharp2.mp3' })
 }
 const B2: Note = {
   name: 'B2',
   pitchClass: PitchClass.B,
-  sound: new Howl({ src: '../sounds/B2.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/B2.mp3' })
 }
 const C3: Note = {
   name: 'C3',
   pitchClass: PitchClass.C,
-  sound: new Howl({ src: '../sounds/C3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/C3.mp3' })
 }
 const CSharp3: Note = {
   name: 'C#3',
   pitchClass: PitchClass.CSharp,
-  sound: new Howl({ src: '../sounds/CSharp3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/CSharp3.mp3' })
 }
 const D3: Note = {
   name: 'D3',
   pitchClass: PitchClass.D,
-  sound: new Howl({ src: '../sounds/D3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/D3.mp3' })
 }
 const DSharp3: Note = {
   name: 'D#3',
   pitchClass: PitchClass.DSharp,
-  sound: new Howl({ src: '../sounds/DSharp3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/DSharp3.mp3' })
 }
 const E3: Note = {
   name: 'E3',
   pitchClass: PitchClass.E,
-  sound: new Howl({ src: '../sounds/E3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/E3.mp3' })
 }
 const F3: Note = {
   name: 'F3',
   pitchClass: PitchClass.F,
-  sound: new Howl({ src: '../sounds/F3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/F3.mp3' })
 }
 const FSharp3: Note = {
   name: 'F#3',
   pitchClass: PitchClass.FSharp,
-  sound: new Howl({ src: '../sounds/FSharp3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/FSharp3.mp3' })
 }
 const G3: Note = {
   name: 'G3',
   pitchClass: PitchClass.G,
-  sound: new Howl({ src: '../sounds/G3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/G3.mp3' })
 }
 const GSharp3: Note = {
   name: 'G#3',
   pitchClass: PitchClass.GSharp,
-  sound: new Howl({ src: '../sounds/GSharp3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/GSharp3.mp3' })
 }
 const A3: Note = {
   name: 'A3',
   pitchClass: PitchClass.A,
-  sound: new Howl({ src: '../sounds/A3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/A3.mp3' })
 }
 const ASharp3: Note = {
   name: 'A#3',
   pitchClass: PitchClass.ASharp,
-  sound: new Howl({ src: '../sounds/ASharp3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/ASharp3.mp3' })
 }
 const B3: Note = {
   name: 'B3',
   pitchClass: PitchClass.B,
-  sound: new Howl({ src: '../sounds/B3.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/B3.mp3' })
 }
 const C4: Note = {
   name: 'C4',
   pitchClass: PitchClass.C,
-  sound: new Howl({ src: '../sounds/C4.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/C4.mp3' })
 }
 const CSharp4: Note = {
   name: 'C#4',
   pitchClass: PitchClass.CSharp,
-  sound: new Howl({ src: '../sounds/CSharp4.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/CSharp4.mp3' })
 }
 const D4: Note = {
   name: 'D4',
   pitchClass: PitchClass.D,
-  sound: new Howl({ src: '../sounds/D4.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/D4.mp3' })
 }
 const DSharp4: Note = {
   name: 'D#4',
   pitchClass: PitchClass.DSharp,
-  sound: new Howl({ src: '../sounds/DSharp4.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/DSharp4.mp3' })
 }
 const E4: Note = {
   name: 'E4',
   pitchClass: PitchClass.E,
-  sound: new Howl({ src: '../sounds/E4.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/E4.mp3' })
 }
 const F4: Note = {
   name: 'F4',
   pitchClass: PitchClass.F,
-  sound: new Howl({ src: '../sounds/F4.mp3' })
+  sound: new Howl({ src: 'src/assets/sounds/F4.mp3' })
 }
 
 const ALL_NOTES: Note[] = [
