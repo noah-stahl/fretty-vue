5fae5f1e1ea03fdeef9c913e494c0572ac176a02
noah-stahl <noah.stahl@mindmatters.de>
2023-07-07 10:47:53 +0200

    restructure QuizView

---
+++ src/constants/Frets/index.ts
@@ -0,0 +1,8 @@
+import { A_FRETS } from "./AFrets";
+import { B_FRETS } from "./BFrets";
+import { D_FRETS } from "./DFrets";
+import { G_FRETS } from "./GFrets";
+import { HIGH_E_FRETS } from "./HighEFrets";
+import { LOW_E_FRETS } from "./LowEFrets";
+
+export { A_FRETS, B_FRETS, D_FRETS, G_FRETS, HIGH_E_FRETS, LOW_E_FRETS }
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,99 +1,29 @@
 <script setup lang="ts">
-import FretButton from "../components/FretButton.vue"
+import FretButton from '../components/FretButton.vue'
+import { A_FRETS, B_FRETS, D_FRETS, G_FRETS, HIGH_E_FRETS, LOW_E_FRETS } from '../constants/Frets'
 </script>
 
 <template>
   <div>
     <h1>Fretboard</h1>
     <div>
-      <FretButton note-name="E" />
-      <FretButton note-name="D#" />
-      <FretButton note-name="D" />
-      <FretButton note-name="C#" />
-      <FretButton note-name="C" />
-      <FretButton note-name="B" />
-      <FretButton note-name="A#" />
-      <FretButton note-name="A" />
-      <FretButton note-name="G#" />
-      <FretButton note-name="G" />
-      <FretButton note-name="F#" />
-      <FretButton note-name="F" />
-      <FretButton note-name="E" />
+      <FretButton v-for="fret in HIGH_E_FRETS" :key="fret.fretNumber" :fret="fret" />
     </div>
     <div>
-      <FretButton note-name="B" />
-      <FretButton note-name="A#" />
-      <FretButton note-name="A" />
-      <FretButton note-name="G#" />
-      <FretButton note-name="G" />
-      <FretButton note-name="F#" />
-      <FretButton note-name="F" />
-      <FretButton note-name="E" />
-      <FretButton note-name="D#" />
-      <FretButton note-name="D" />
-      <FretButton note-name="C#" />
-      <FretButton note-name="C" />
-      <FretButton note-name="B" />
+      <FretButton v-for="fret in B_FRETS" :key="fret.fretNumber" :fret="fret" />
     </div>
     <div>
-      <FretButton note-name="G" />
-      <FretButton note-name="F#" />
-      <FretButton note-name="F" />
-      <FretButton note-name="E" />
-      <FretButton note-name="D#" />
-      <FretButton note-name="D" />
-      <FretButton note-name="C#" />
-      <FretButton note-name="C" />
-      <FretButton note-name="B" />
-      <FretButton note-name="A#" />
-      <FretButton note-name="A" />
-      <FretButton note-name="G#" />
-      <FretButton note-name="G" />
+      <FretButton v-for="fret in G_FRETS" :key="fret.fretNumber" :fret="fret" />
     </div>
     <div>
-      <FretButton note-name="D" />
-      <FretButton note-name="C#" />
-      <FretButton note-name="C" />
-      <FretButton note-name="B" />
-      <FretButton note-name="A#" />
-      <FretButton note-name="A" />
-      <FretButton note-name="G#" />
-      <FretButton note-name="G" />
-      <FretButton note-name="F#" />
-      <FretButton note-name="F" />
-      <FretButton note-name="E" />
-      <FretButton note-name="D#" />
-      <FretButton note-name="D" />
+      <FretButton v-for="fret in D_FRETS" :key="fret.fretNumber" :fret="fret" />
     </div>
     <div>
-      <FretButton note-name="A" />
-      <FretButton note-name="G#" />
-      <FretButton note-name="G" />
-      <FretButton note-name="F#" />
-      <FretButton note-name="F" />
-      <FretButton note-name="E" />
-      <FretButton note-name="D#" />
-      <FretButton note-name="D" />
-      <FretButton note-name="C#" />
-      <FretButton note-name="C" />
-      <FretButton note-name="B" />
-      <FretButton note-name="A#" />
-      <FretButton note-name="A" />
+      <FretButton v-for="fret in A_FRETS" :key="fret.fretNumber" :fret="fret" />
     </div>
     <div>
-      <FretButton note-name="E" />
-      <FretButton note-name="D#" />
-      <FretButton note-name="D" />
-      <FretButton note-name="C#" />
-      <FretButton note-name="C" />
-      <FretButton note-name="B" />
-      <FretButton note-name="A#" />
-      <FretButton note-name="A" />
-      <FretButton note-name="G#" />
-      <FretButton note-name="G" />
-      <FretButton note-name="F#" />
-      <FretButton note-name="F" />
-      <FretButton note-name="E" />
+      <FretButton v-for="fret in LOW_E_FRETS" :key="fret.fretNumber" :fret="fret" />
     </div>
   </div>
 </template>
+../constants/Frets
