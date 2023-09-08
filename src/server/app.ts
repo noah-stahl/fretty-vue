96b96e48aa6637e78a2a07886997a9eb9375fa4e
noah-stahl <noah.stahl@mindmatters.de>
2023-09-08 11:12:08 +0200

    Add prisma

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 import type { Express, Request, Response } from 'express'
+import { prisma } from './prisma'
 
 const app: Express = express()
 const port = 5001
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/users', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
 })
 
 app.get('/food', (req: Request, res: Response) => {
